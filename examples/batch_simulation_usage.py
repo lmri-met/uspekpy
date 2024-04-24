8daa5e0c14ab458279a755b5f0796375aa48cdee
Xandra Campo <xkmpera@gmail.com>
2024-04-24 07:49:45 +0200

    Add TODOs

--- examples/batch_simulation_usage.py
+++ examples/batch_simulation_usage.py
@@ -4,7 +4,8 @@ from uspeckpy.simulation import batch_simulation
 
 # Using batch_simulation
 # ----------------------------------------------------------------------------------------------------------------------
-
+# TODO: units of mu_tr_rho
+# TODO: check uncertainty type for np.random.normal()
 my_excel = Path('data/input/input.xlsx')
 my_sheet = 'input'
 my_folder = Path('/scratches')
