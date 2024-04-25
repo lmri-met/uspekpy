897e02a3bed5d389c2bcfb203373b30d14935da8
Xandra Campo <xkmpera@gmail.com>
2024-04-25 13:00:34 +0200

    Change input file paths to work on any PC

--- examples/batch_simulation_usage.py
+++ examples/batch_simulation_usage.py
@@ -18,11 +18,11 @@ my_excel = 'data/input/input.xlsx'
 my_sheet = 'input'
 
 # Define the output folder path where the simulation results will be saved
-my_folder = 'scratches'
+my_folder = 'output'
 
 # Call the batch_simulation function with the provided input arguments
 # and store the resulting DataFrame in df1
-# df1 = batch_simulation(input_file_path=my_excel, output_folder=my_folder, sheet_name=my_sheet)
+df1 = batch_simulation(input_file_path=my_excel, output_folder=my_folder, sheet_name=my_sheet)
 
 # Using batch_simulation() with CSV file input
 # ----------------------------------------------------------------------------------------------------------------------
