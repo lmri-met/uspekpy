a83d17ed5dbcf58d9dbccd3987f1af4195cfff1d
Xandra Campo <xkmpera@gmail.com>
2024-04-26 13:00:20 +0200

    Allow user to define output file name

--- examples/batch_simulation_from_excel.py
+++ examples/batch_simulation_from_excel.py
@@ -12,9 +12,12 @@ my_sheet = 'input'
 # Define the output folder path where the simulation results will be saved
 my_folder = 'output'
 
+# Define the name of the output file where the simulation results will be saved
+my_output_csv = 'output.csv'
+
 # Call the batch_simulation function with the provided input arguments
 # and store the resulting DataFrame in df
 df = batch_simulation(input_file_path=my_excel, sheet_name=my_sheet)
 
 # Save results to a CSV file
-df.to_csv(Path(my_folder) / 'output.csv', index=True)
+df.to_csv(Path(my_folder) / my_output_csv, index=True)
