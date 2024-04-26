dbd35f8af46fc49e21fa7b8506aa5a9bf41bd79f
Xandra Campo <xkmpera@gmail.com>
2024-04-26 09:41:32 +0200

    Add docstrings to SpekWrapper class

--- uspeckpy/uspekpy.py
+++ uspeckpy/uspekpy.py
@@ -6,40 +6,94 @@ from spekpy import Spek
 
 
 class SpecWrapper(Spek):
+    """
+    A subclass of Spek representing a spectrum with additional methods for computation of mean quantities.
+
+    Parameters:
+    - kvp (float): The peak kilovoltage (kVp) of the x-ray tube.
+    - th (float): The anode angle of the x-ray tube.
+
+    Attributes:
+    Inherits kvp and th attributes from Spek class.
+
+    Methods:
+    - get_mean_energy(): Compute the mean energy of the spectrum.
+    - get_mean_kerma(mass_transmission_coefficients): Compute the mean kerma of the spectrum using mass transmission
+      coefficients.
+    - get_mean_conversion_coefficient(mass_transmission_coefficients, conversion_coefficients, angle=None):
+      Compute the mean conversion coefficient of the spectrum using mass transmission coefficients and conversion
+      coefficients.
+    """
     def __init__(self, kvp, th):
-        # Initialize SpecWrapper as a subclass of Spek with kvp and th
+        """
+        Initialize SpecWrapper instance with peak kilovoltage anode angle of the x-ray tube.
+        """
         Spek.__init__(self, kvp, th)
 
     def get_mean_energy(self):
-        # Method to compute the mean energy of the spectrum
+        """
+        Compute the mean energy of the spectrum.
+
+        Returns:
+        float: The mean energy of the spectrum.
+        """
         # Get spectrum energy and fluence
         energy, fluence = self.get_spectrum(edges=False)
+
         # Compute mean energy
         return sum(fluence * energy) / fluence.sum()
 
     def get_mean_kerma(self, mass_transmission_coefficients):
-        # Method to compute the mean kerma using mass transmission coefficients
+        """
+        Compute the mean kerma using mass transmission coefficients.
+
+        Parameters:
+        mass_transmission_coefficients (tuple): Tuple containing the energies and values of the mass transmission
+        coefficients.
+
+        Returns:
+        float: The mean kerma computed.
+        """
         # Get spectrum energy and fluence
         energy, fluence = self.get_spectrum(edges=False)
-        # Unpack energy_mu and mu from mass_transmission_coefficients
+
+        # Unpack the energies and values of the mass transmission coefficients
         energy_mu, mu = dg.parse_mass_transmission_coefficients(mass_transmission_coefficients)
-        # Interpolate mass attenuation coefficients for the given energy spectrum in logarithmic scale
+
+        # Interpolate mass attenuation coefficients for the spectrum energies in logarithmic scale
         interpolated_mu = interpolate(x=energy_mu, y=mu, new_x=energy)
+
         # Compute mean kerma
         return sum(fluence * energy * interpolated_mu) / fluence.sum()
 
     def get_mean_conversion_coefficient(self, mass_transmission_coefficients, conversion_coefficients, angle=None):
-        # Method to compute the mean conversion coefficient using mass transmission coefficients
+        """
+        Compute the mean conversion coefficient using mass transmission coefficients and conversion coefficients.
+
+        Parameters:
+        mass_transmission_coefficients (tuple): Tuple containing the energies and values of the mass transmission
+        coefficients.
+        conversion_coefficients (tuple): Tuple containing the energies and values of the conversion coefficients.
+        angle (float, optional): The irradiation angle at which conversion coefficients are calculated.
+
+        Returns:
+        float: The mean conversion coefficient computed.
+        """
         # Get spectrum energy and fluence
         energy, fluence = self.get_spectrum(edges=False)
-        # Unpack energy_mu and mu from mass_transmission_coefficients
+
+        # Unpack the energies and values of the mass transmission coefficients
         energy_mu, mu = dg.parse_mass_transmission_coefficients(mass_transmission_coefficients)
-        # Unpack energy_hk and hk from mono_energetic_conversion_coefficients
+
+        # Unpack the energies and values of the conversion coefficients
         energy_hk, hk = dg.parse_conversion_coefficients(conversion_coefficients, angle)
-        # Interpolate mass attenuation coefficients for the given energy spectrum in logarithmic scale
+
+        # Interpolate mass attenuation coefficients for the spectrum energies in logarithmic scale
         interpolated_mu = interpolate(x=energy_mu, y=mu, new_x=energy)
-        # Interpolate mono energetic conversion coefficients for the given energy spectrum in logarithmic scale
+
+        # Interpolate conversion coefficients for the spectrum energies in logarithmic scale
         interpolated_hk = interpolate(x=energy_hk, y=hk, new_x=energy)
+
         # Compute mean conversion coefficient
         return sum(fluence * energy * interpolated_mu * interpolated_hk) / fluence.sum()
 
