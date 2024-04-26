0f22f5363633f26c78d0ede212736bb033a0def3
Xandra Campo <xkmpera@gmail.com>
2024-04-26 11:21:25 +0200

    Add docstrings of to digest and simulation modules

--- uspeckpy/digest.py
+++ uspeckpy/digest.py
@@ -6,52 +6,88 @@ import pandas as pd
 
 
 def parse_mass_transmission_coefficients(coefficients):
-    # Get mass transmission coefficients in the format required by SpekWrapper (tuple of two numpy arrays)
+    """
+    Parse mass transmission coefficients into the required format by SpekWrapper.
 
+    This function takes mass transmission coefficients in various formats and converts them into the format required
+    by SpekWrapper, which is a tuple containing two numpy arrays representing the energies and mass transmission
+    coefficients, respectively.
+
+    Args:
+    coefficients (tuple or str): Mass transmission coefficients. This can be either a tuple of two numpy arrays
+        representing energies and mass transmission coefficients, or a string representing the file path of a CSV
+        file containing two columns: energy and mass transmission coefficients.
+
+    Returns:
+    tuple: A tuple containing two numpy arrays representing energies and mass transmission coefficients, respectively.
+
+    Raises:
+    ValueError: If the input format of mass transmission coefficients is not supported.
+    """
+    # Check if the input is already in the required format (tuple of two arrays)
     if is_tuple_of_two_arrays(coefficients):
         return coefficients
+
+    # If the input is a CSV file with two columns
     elif is_csv_with_two_columns(coefficients):
-        # The mass transmission coefficients CSV file should have only 2 columns, the first one for the energy and the
-        # second one for the mass transmission coefficients.
-        # Load CSV file into numpy array
+        # Load CSV file into a numpy array, skipping the header
         array2d = np.genfromtxt(coefficients, delimiter=',', skip_header=1, unpack=True)
         # Build tuple of mass transmission coefficients
         return array2d[0], array2d[1]
     else:
+        # If the input format is not supported, raise a ValueError
         raise ValueError(f"Unsupported mass transmission coefficients format. Only a tuple of two numpy arrays and "
                          f"a CSV file with two columns are supported.")
 
 
 def parse_conversion_coefficients(coefficients, irradiation_angle):
-    # Get conversion coefficients in the format required by SpekWrapper (tuple of two numpy arrays)
+    """
+    Parse conversion coefficients into the required format by SpekWrapper.
+
+    This function takes conversion coefficients in various formats and converts them into the format required
+    by SpekWrapper, which is a tuple containing two numpy arrays representing the energies and conversion
+    coefficients, respectively.
+
+    Args:
+    coefficients (tuple or str): Conversion coefficients. This can be either a tuple of two numpy arrays
+        representing energies and conversion coefficients, or a string representing the file path of a CSV
+        file containing at least two columns: energy and conversion coefficients.
+    irradiation_angle (str): Irradiation angle for which the conversion coefficients are calculated.
+
+    Returns:
+    tuple: A tuple containing two numpy arrays representing energies and conversion coefficients, respectively.
+
+    Raises:
+    ValueError: If the input format of conversion coefficients is not supported.
+
+    """
+    # Check if the input is already in the required format (tuple of two arrays)
     if is_tuple_of_two_arrays(coefficients):
         return coefficients
+    # If the input is a valid CSV file
     elif is_valid_csv(coefficients):
-        # Read CSV file into DataFrame
+        # Read CSV file into a DataFrame
         df = pd.read_csv(coefficients)
 
-        # Get the energies of the mono-energetic conversion coefficients (always the first column of the Dataframe)
+        # Get the energies from the first column of the DataFrame
         energies = df.iloc[:, 0].values
 
-        # If the conversion coefficients Dataframe has only 2 columns, the first is the energy and the second is the
-        # conversion coefficients.
+        # If the DataFrame has only 2 columns, the second column contains the conversion coefficients
         if df.shape[1] == 2:
-            # Get the values of the mono-energetic conversion coefficients (the second column of the Dataframe)
             values = df.iloc[:, 1].values
-        # If the conversion coefficients Dataframe has more than 2 columns, then it contains conversion coefficients for
-        # more than one irradiation angle.
-        else:  # TODO: check with multicolumn input file
-            # Get the label of the column which contains the irradiation angle
+        else:
+            # Find the column containing the conversion coefficients corresponding to the specified irradiation angle
             column_label = next((label for label in df.columns if irradiation_angle in label), None)
 
-            # Get the values of the mono-energetic conversion coefficients (the second column of the Dataframe)
+            # Get the conversion coefficients from the identified column
             values = df.loc[:, column_label].values
 
-        # Build tuple of conversion coefficients in the format required by SpekWrapper (tuple of two numpy arrays)
+        # Build a tuple of conversion coefficients in the required format (tuple of two numpy arrays)
         return energies, values
     else:
-        raise ValueError(f"Unsupported conversion coefficients format. Only a tuple of two numpy arrays and "
-                         f"a CSV file with two or more columns are supported.")
+        # If the input format is not supported, raise a ValueError
+        raise ValueError("Unsupported conversion coefficients format. Only a tuple of two numpy arrays and "
+                         "a CSV file with two or more columns are supported.")
 
 
 def parse_beam_parameters(df, column):
@@ -74,16 +110,37 @@ def parse_beam_parameters(df, column):
 
 
 def output_digest(input_df, output_dfs):
-    # Build a DataFrame with the simulations result in the format of the input DataFrame
+    """
+    Generate a DataFrame combining input and simulation results.
+
+    This function generates a DataFrame by combining the input DataFrame with the simulation
+    results. It transforms the simulation results into a format suitable for merging with the input DataFrame
+    and concatenates them accordingly.
+
+    Args:
+    input_df (pandas.DataFrame): Input DataFrame containing simulation parameters.
+    output_dfs (list of pandas.DataFrame): List of DataFrames containing simulation results.
+
+    Returns:
+    pandas.DataFrame: DataFrame combining input and simulation results.
+
+    """
+    # Define columns to extract from the output DataFrames containing simulation results
     result_columns = ['HVL1 Al', 'HVL2 Al', 'HVL1 Cu', 'HVL2 Cu', 'Mean energy', 'Mean kerma',
                       'Mean conv. coefficient.']
+
+    # Define rows to extract from the output DataFrames containing simulation results
     result_rows = ['Mean', 'Standard deviation', 'Relative uncertainty']
+
+    # Initialize an empty list to store transformed simulation results
     results = []
+
+    # Iterate over each output DataFrame containing simulation results
     for output_df in output_dfs:
-        # Set the index of the DataFrame to a column named '#', modifying the DataFrame in place
+        # Set the index of the DataFrame to '#' column
         output_df.set_index(keys='#', inplace=True)
 
-        # Extract a subset of data from the DataFrame using specific rows and columns
+        # Extract a subset of data from the DataFrame based on result_rows and result_columns
         df = output_df.loc[result_rows, result_columns]
 
         # Transpose the resulting DataFrame to swap rows and columns
@@ -101,47 +158,109 @@ def output_digest(input_df, output_dfs):
         # Set the combined index to be the new index of the DataFrame
         df = df.set_index(combined_index)
 
+        # Append the transformed DataFrame to the results list
         results.append(df)
 
-    # Merge the results DataFrames on their indices using reduce
+    # Merge all transformed DataFrames using reduce and merge function
     merged_df = reduce(lambda left, right: pd.merge(left, right, left_index=True, right_index=True), results)
-    merged_df.columns = input_df.columns
-    # df = pd.DataFrame(data=rows2)
 
-    # Create DataFrame with one row using a list
+    # Set the columns of the merged DataFrame to match the input DataFrame columns
+    merged_df.columns = input_df.columns
+
+    # Create a row to indicate the results section of the merged DataFrame
     data = [['Results'] + [None] * input_df.shape[1]]
+
+    # Define columns for the DataFrame
     columns = ['Name'] + list(input_df.columns)
+
+    # Create a DataFrame from data with columns specified
     df = pd.DataFrame(data, columns=columns)
+
+    # Set the index of the DataFrame to 'Name'
     df.set_index(keys='Name', inplace=True)
 
-    # Concatenate along rows
+    # Concatenate the input DataFrame, the results section DataFrame and the merged results DataFrame
     return pd.concat([input_df, df, merged_df])
 
 
 def is_tuple_of_two_arrays(arg):
+    """
+    Check if the input argument is a tuple containing two numpy arrays.
+
+    This function verifies if the input argument is a tuple containing exactly two numpy arrays.
+    If the input argument meets the criteria, the function returns True; otherwise, it returns False.
+
+    Args:
+    arg (any): The input argument to be validated.
+
+    Returns:
+    bool: True if the input argument is a tuple containing two numpy arrays, False otherwise.
+    """
+    # Check if the input is a tuple
     if not isinstance(arg, tuple):
         return False
+
+    # Check if the tuple contains exactly two elements
     if len(arg) != 2:
         return False
+
+    # Check if both elements of the tuple are numpy arrays
     if not isinstance(arg[0], np.ndarray) or not isinstance(arg[1], np.ndarray):
         return False
+
+    # If all conditions are met, return True
     return True
 
 
 def is_csv_with_two_columns(filename):
+    """
+    Check if a CSV file has exactly two columns.
+
+    This function reads the provided CSV file and checks if each row contains exactly two columns.
+    If any row has a different number of columns, the function returns False.
+    If all rows have exactly two columns, the function returns True.
+
+    Args:
+    filename (str): The path to the CSV file to be validated.
+
+    Returns:
+    bool: True if the CSV file has exactly two columns in each row, False otherwise.
+    """
+    # Check if the file has a CSV extension
     if not filename.lower().endswith('.csv'):
         return False
+
+    # Open the CSV file
     with open(filename, 'r') as file:
         rows = reader(file)
+
+        # Iterate over each row in the CSV file
         for row in rows:
+            # If any row does not contain exactly two columns, return False
             if len(row) != 2:
                 return False
+        # If all rows contain exactly two columns, return True
         return True
 
 
 def is_valid_csv(filename):
+    """
+    Check if a CSV file is valid by ensuring that all rows have the same length.
+
+    This function reads the provided CSV file and checks if all rows have the same length.
+    If any row has a different length from the first row, the function returns False.
+    If all rows have the same length, the function returns True, indicating that the CSV file is valid.
+
+    Args:
+    filename (str): The path to the CSV file to be validated.
+
+    Returns:
+    bool: True if the CSV file is valid (all rows have the same length), False otherwise.
+    """
+    # Check if the file has a CSV extension
     if not filename.lower().endswith('.csv'):
         return False
+
     # Open the CSV file
     with open(filename, 'r') as file:
         rows = reader(file)
@@ -151,6 +270,8 @@ def is_valid_csv(filename):
 
         # Check the length of each subsequent row
         for row in rows:
+            # If any row has a different length, return False
             if len(row) != first_row_length:
-                return False  # If any row has a different length, return False
-        return True  # If all rows have the same length, return True
+                return False
+        # If all rows have the same length, return True
+        return True
--- uspeckpy/simulation.py
+++ uspeckpy/simulation.py
@@ -6,18 +6,34 @@ import uspeckpy.digest as dg
 from uspeckpy.uspekpy import USpek
 
 
-def batch_simulation(input_file_path, output_folder, sheet_name=None):
+def batch_simulation(input_file_path, sheet_name=None):
+    """
+    Perform batch simulations and generate output data.
+
+    This function performs batch simulations based on input data provided in an Excel or CSV file.
+    It reads the input data into a DataFrame, processes each simulation, and generates output data.
+    The output data is stored in a list of DataFrames, one for each simulation, and returned. TODO
+
+    Args:
+    input_file_path (str): The path to the input file containing simulation parameters.
+    sheet_name (str, optional): The name of the sheet to read if the input file is in Excel format.
+
+    Returns:
+    list: A list of DataFrames containing the output data for each simulation. TODO
+
+    Raises:
+    ValueError: If the input file format is not supported or if there are issues with the input data.
+    """
     # Print a message indicating the start of input digestion
     print('Batch simulation')
 
-    # Print a message indicating the start of input digestion
+    # Print a message indicating the start of initial input digestion
     print('\nInitial input digest')
 
     # Read input Excel or CSV file into a DataFrame
     input_df = read_file_to_dataframe(input_file_path, sheet_name=sheet_name)
 
     # Read Excel file into a DataFrame and set 'Name' column as index
-    # input_df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
     input_df.set_index(keys='Name', inplace=True)
 
     # Initialize an empty list to store the simulations results
@@ -26,6 +42,7 @@ def batch_simulation(input_file_path, output_folder, sheet_name=None):
     # Initialize an iterator
     i = 0
 
+    # Iterate over each column in the input DataFrame
     for column_name in input_df.columns:
         # Print a message indicating simulation number
         print(f'\nSimulation {i + 1}')
@@ -33,43 +50,41 @@ def batch_simulation(input_file_path, output_folder, sheet_name=None):
         # Print a message indicating the start of input digestion
         print('Input digest')
 
-        # Get beam parameters in the format required by SpekWrapper (dictionary of tuples)
+        # Parse beam parameters from the input DataFrame
         beam_parameters = dg.parse_beam_parameters(df=input_df, column=column_name)
 
-        # Extract mass transmission coefficients CSV file path from input DataFrame column
+        # Extract mass transmission coefficients file path from the input DataFrame
         file_path = input_df.at['Mass transmission coefficients file', column_name]
 
-        # Get mass transmission coefficients in the format required by SpekWrapper (tuple of two numpy arrays)
+        # Parse mass transmission coefficients from the file
         mass_transmission_coefficients = dg.parse_mass_transmission_coefficients(coefficients=file_path)
 
-        # Extract conversion coefficients CSV file path from input DataFrame column
+        # Extract mono-energetic conversion coefficients file path from the input DataFrame
         file_path = input_df.at['Mono-energetic conversion coefficients file', column_name]
 
-        # Extract irradiation angle from the input DataFrame column
+        # Extract irradiation angle from the input DataFrame
         irradiation_angle = input_df.at['Irradiation angle (deg)', column_name]
 
-        # Get conversion coefficients in the format required by SpekWrapper (tuple of two numpy arrays)
+        # Parse conversion coefficients from the file
         conversion_coefficients = dg.parse_conversion_coefficients(coefficients=file_path,
                                                                    irradiation_angle=irradiation_angle)
 
-        # Extract number of simulations from the input DataFrame column
+        # Extract number of simulations from the input DataFrame
         simulations_number = input_df.at['Number of simulations', column_name]
 
-        # Extract mass transmission coefficients uncertainty from the input DataFrame column
+        # Extract mass transmission coefficients uncertainty from the input DataFrame
         mass_transmission_coefficients_uncertainty = input_df.at['Mass transmission coefficients uncertainty', column_name]
 
         # Print a message indicating the start of input digestion
         print('Simulation')
 
-        # Create USpekPy object with given beam parameters, mass transmission coefficients and conversion coefficients
+        # Create a USpek object with the specified parameters
         s = USpek(beam_parameters=beam_parameters, mass_transmission_coefficients=mass_transmission_coefficients,
                   mass_transmission_coefficients_uncertainty=mass_transmission_coefficients_uncertainty,
                   conversion_coefficients=conversion_coefficients)
 
-        # Run simulation with a given number of iterations
+        # Run simulation with the specified number of iterations
         output_df = s.simulate(simulations_number=simulations_number)
-
-        output_df.to_csv(Path(output_folder) / f'output_case{i + 1}.csv', index=True)
 
         # Print a message indicating the start of input digestion
         print('Output digest')
@@ -77,27 +92,34 @@ def batch_simulation(input_file_path, output_folder, sheet_name=None):
         # Append output DataFrame to output DataFrames list
         output_dfs.append(output_df)
 
-        # Increment iterator
+        # Increment the simulation iterator
         i += 1
 
     # Print a message indicating the start of input digestion
     print('\nFinal output digest')
 
-    # Return DataFrame with the results
+    # Return DataFrame with the simulation results
     return dg.output_digest(input_df=input_df, output_dfs=output_dfs)
 
 
 def read_file_to_dataframe(file_path, sheet_name=None):
     """
-    Read an Excel or CSV file into a DataFrame.
+    Read data from a file into a pandas DataFrame.
 
-    Parameters:
-        file_path (str): The path to the input file.
-        sheet_name (str or int, default None): The name or index of the sheet to read if file_path is an Excel.
-            If None, reads all sheets to a dictionary.
+    This function reads data from a file specified by the file_path parameter into a pandas DataFrame.
+    The file can be either in Excel (.xlsx, .xls) or CSV (.csv) format. If the file is in Excel format,
+    the optional sheet_name parameter can be used to specify the sheet name to read. If the file is in CSV
+    format, the function automatically detects the delimiter.
+
+    Args:
+    file_path (str): The path to the file to be read.
+    sheet_name (str, optional): The name of the sheet to read if the file is in Excel format.
 
     Returns:
-        pandas.DataFrame: The DataFrame containing the data from the file.
+    pandas.DataFrame: A DataFrame containing the data read from the file.
+
+    Raises:
+    ValueError: If the file format is not supported. Only Excel (.xlsx, .xls) and CSV (.csv) files are supported.
     """
     # Check the file extension to determine the file type
     if file_path.endswith('.xlsx') or file_path.endswith('.xls'):
@@ -120,6 +142,7 @@ def read_file_to_dataframe(file_path, sheet_name=None):
                         # If conversion to float also fails, leave it unchanged
                         pass
     else:
+        # Raise a ValueError for unsupported file formats
         raise ValueError("Unsupported file format. Only Excel (.xlsx, .xls) and CSV (.csv) files are supported.")
 
     return df
