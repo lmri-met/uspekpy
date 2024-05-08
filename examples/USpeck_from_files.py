fdaf7c6b294621c26fe31a608ea005cd812958de
pazaviles <163848632+pazaviles@users.noreply.github.com>
2024-05-08 11:15:21 +0200

    Update USpeck_from_files.py
    Revision of docstrings

--- examples/USpeck_from_files.py
+++ examples/USpeck_from_files.py
@@ -15,27 +15,27 @@ my_beam = {
     'Air': (1000, 0.01)
 }
 
-# Define mass transmission coefficients
+# Defines mass energy transfer coefficients of air
 my_mu_csv = 'data/mu_tr_rho.csv'
 
-# Define conversion coefficients
+# Defines conversion coefficients
 my_hk_csv = 'data/h_k_h_amb_10.csv'
 
-# Define mass transmission coefficients relative uncertainty (k=1)
+# Defines relative uncertainty (k=1) on mass energy transfer coefficients of air 
 my_mu_std = 0.01
 
-# Create USpekPy object with given beam parameters, mass transmission coefficients and conversion coefficients
+# Create USpekPy object with given beam parameters, mass energy transfer coefficients of air and conversion coefficients
 s = USpek(beam_parameters=my_beam, mass_transmission_coefficients=my_mu_csv,
           mass_transmission_coefficients_uncertainty=my_mu_std, conversion_coefficients=my_hk_csv)
 
-# Run simulation with a given number of iterations
+# Runs simulation with a given number of iterations
 df = s.simulate(simulations_number=3)
 
-# Define the output folder path where the simulation results will be saved
+# Defines the output folder path where the simulation results will be saved
 my_folder = 'output'
 
-# Define the name of the output file where the simulation results will be saved
+# Defines the name of the output file where the simulation results will be saved
 my_output_csv = 'output.csv'
 
-# Save results to a CSV file
+# Saves results to a CSV file
 df.to_csv(Path(my_folder) / my_output_csv, index=True)
