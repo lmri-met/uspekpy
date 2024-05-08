49085ed9d9a9dad7d66d0a7cb7ac410950f75edc
pazaviles <163848632+pazaviles@users.noreply.github.com>
2024-05-08 11:13:12 +0200

    Update SpekWrapper_from_files.py
    Revision of docstrings

--- examples/SpekWrapper_from_files.py
+++ examples/SpekWrapper_from_files.py
@@ -1,7 +1,7 @@
 # Scripto to demonstrate the usage of SpekWrapper class with CSV input files
 from uspeckpy.wrapper import SpekWrapper
 
-# Define x-ray beam parameters
+# Defines x-ray beam parameters
 my_filters = [
     ('Al', 4),
     ('Cu', 0.6),
@@ -11,13 +11,13 @@ my_filters = [
     ('Air', 1000)
 ]
 
-# Define mass transmission coefficients
+# Defines mass energy transfer coefficients of air
 my_mu_csv = 'data/mu_tr_rho.csv'
 
-# Define conversion coefficients
+# Defines conversion coefficients
 my_hk_csv = 'data//h_k_h_amb_10.csv'
 
-# Initialize an SpeckWrapper object and add filters
+# Initialises an SpeckWrapper object and add filters
 spectrum = SpekWrapper(kvp=60, th=20)
 spectrum.multi_filter(my_filters)
 
@@ -27,14 +27,14 @@ hvl2_al = spectrum.get_hvl2()
 hvl1_cu = spectrum.get_hvl1(matl='Cu')
 hvl2_cu = spectrum.get_hvl2(matl='Cu')
 
-# Get mean energy
+# Gets mean energy
 mean_energy = spectrum.get_mean_energy()
 mean_energy_SpekPy = spectrum.get_emean()
 
-# Get mean kerma
+# Gets mean air kerma
 mean_kerma_SpekPy = spectrum.get_kerma()
 mean_kerma = spectrum.get_mean_kerma(mass_transmission_coefficients=my_mu_csv)
 
-# Get mean conversion coefficient
+# Gets mean conversion coefficient
 mean_hk = spectrum.get_mean_conversion_coefficient(mass_transmission_coefficients=my_mu_csv,
                                                    conversion_coefficients=my_hk_csv)
